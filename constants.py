8a635b51a9b6f886e3ef039471648ff841134b4e
Charptr0 <charlesofficial1@hotmail.com>
2021-08-07 14:57:24 -0400

    added init function

--- constants.py
+++ constants.py
@@ -1,14 +1,16 @@
 #titles and prompts
 MAIN_MENU_PROMPT = '''=======================================
 Local Password Manager
-=======================================
-'''
+======================================='''
 
 MAIN_MENU_CHOICES = '''1. Login
 2. Create a new user
 3. Delete a user
 4. Quit
-'''
+Input: '''
+
+
+UNKNOWN_CHOICE_ERR = "Unknown choice, please try again\n"
 
 MAIN_LOGIN_USERNAME_PROMPT = '''Type "!q" to terminate the process
 Master Username: '''
@@ -21,3 +23,7 @@ MAIN_MENU_LOGIN = 1
 MAIN_MENU_CREATE_NEW_USER = 2
 MAIN_MENU_DELETE_USER = 3
 MAIN_MENU_QUIT = 4
+
+##############################################################################################################
+#all collections of users
+LOGIN_DATABASE_NAME = "logins"
--- database.py
+++ database.py
@@ -1,1 +1,27 @@
 import sqlite3
+from sqlite3.dbapi2 import Error
+from constants import *
+
+LOGIN_PATH = "users/" + LOGIN_DATABASE_NAME + ".db"
+
+def init():
+    connection = sqlite3.connect(LOGIN_PATH)
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute('''CREATE TABLE IF NOT EXISTS users(
+            username TEXT NOT NULL,
+            pasword TEXT NOT NULL,
+            date_of_creation TEXT NOT NULL
+        )''')
+
+        connection.commit()
+
+    except Error as e: print(e)
+    finally: connection.close()
+
+def isValidLogin(input_username, input_password):
+    pass
+
+
+init()
