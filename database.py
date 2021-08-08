cde4a213938f15ca9863ec82038b9cda2d559e22
Charptr0 <charlesofficial1@hotmail.com>
2021-08-08 14:52:24 -0400

    remove obscure code in isValidLogin()

--- database.py
+++ database.py
@@ -1,17 +1,21 @@
 import sqlite3
 import os
-from sqlite3.dbapi2 import Error, connect
+from sqlite3.dbapi2 import Error
 from constants import *
+from datetime import datetime
 
 LOGIN_PATH = "users/" + LOGIN_DATABASE_NAME + ".db"
 
-def init():
-    if not os.path.exists("users/"): os.makedirs("users/")
+def getCurrentTime(): return datetime.today().strftime('%Y-%m-%d')
 
+def init():
+    if not os.path.exists("users/"): os.makedirs("users/") #create the user/ dir if not exist
+
+    #secure a connection to the database
     connection = sqlite3.connect(LOGIN_PATH)
     cursor = connection.cursor()
 
-    try:
+    try: #try to create the table
         cursor.execute('''CREATE TABLE IF NOT EXISTS users(
             username TEXT NOT NULL,
             password TEXT NOT NULL,
@@ -20,30 +24,46 @@ def init():
 
         connection.commit()
 
-    except Error as e: print(e)
-    finally: connection.close()
+    except Error as e: print(e) #if err
+    finally: connection.close() #close the connection
 
+#check if the input username and password matches the record in the database
 def isValidLogin(input_username, input_password):
     connection = sqlite3.connect(LOGIN_PATH)
     cursor = connection.cursor()
 
-    try:
+    try: #check for username
         cursor.execute('''SELECT username, password FROM users WHERE username="{}"'''.format(input_username))
-        user_username = cursor.fetchall()
+        user = cursor.fetchall()
     except Error as e: print(e)
+    finally: connection.close()
     
-    if len(user_username) == 0: return False, ERR_INVALID_USERNAME
-    if len(user_username) > 1: return False, ERR_USERNAME_ALREADY_EXIST
+    if len(user) == 0: return ERR_INVALID_USERNAME #no such username exist
+    if user[0][1] != input_password: return ERR_INVALID_PASSWORD #password does not match
+
+    return NO_ERR
+
+def addNewLogin(input_username, input_password):
+    connection = sqlite3.connect(LOGIN_PATH)
+    cursor = connection.cursor()
 
     try:
-        cursor.execute('''SELECT username, password FROM users WHERE password="{}"'''.format(input_password))
-        user_password = cursor.fetchall()
+        cursor.execute('''SELECT username FROM users WHERE username="{}"'''.format(input_username))
+        data = cursor.fetchall()
     except Error as e: print(e)
 
-    if len(user_password) == 0: return False, ERR_INVALID_PASSWORD
+    if len(data) > 0: 
+        connection.close()
+        return ERR_USERNAME_ALREADY_EXIST
 
-    connection.close()
+    try:
+        cursor.execute('''INSERT INTO users(username, 
+        password, 
+        date_of_creation) VALUES (?, ?, ?)''', [input_username, input_password, getCurrentTime()])
 
-    return True, NO_ERR
+        connection.commit()
+        print("Entry has been successfully added to the database")
+    except Error as e: print(e)
+    finally: connection.close()
 
-init()
+    return NO_ERR
